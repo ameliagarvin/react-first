cae9f6752d83b34a784b9b17abc653369f0e216a
Amelia <ameliagarvin1@gmail.com>
2020-01-01 16:18:53 -0800

    Updates

---
+++ precache-manifest.85e4806df493897702aeb414c82bf1fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "029e0e6e90bd0cad218da0c867e8c762",
+    "url": "ameliagarvin.github.io/monsters/index.html"
+  },
+  {
+    "revision": "fcab077b2cd6380aecb1",
+    "url": "ameliagarvin.github.io/monsters/static/css/main.493d3263.chunk.css"
+  },
+  {
+    "revision": "e681d63e092c05d48422",
+    "url": "ameliagarvin.github.io/monsters/static/js/2.7699bed9.chunk.js"
+  },
+  {
+    "revision": "fcab077b2cd6380aecb1",
+    "url": "ameliagarvin.github.io/monsters/static/js/main.cc5568ad.chunk.js"
+  },
+  {
+    "revision": "9ae8b8ff2e311be27f6c",
+    "url": "ameliagarvin.github.io/monsters/static/js/runtime-main.1fa2d762.js"
+  }
+]);
